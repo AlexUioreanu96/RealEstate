3bd7a96cc8d7ab45f200e43381694a3c2f7684e5
Alex Uioreanu <alexandru.uioreanu@gmail.com>
2022-09-08 09:47:07 +0300

    Adding fonts and creating some styles

--- app/src/main/java/com/example/realestate/MainScreen.kt
+++ app/src/main/java/com/example/realestate/MainScreen.kt
@@ -1,16 +1,18 @@
 package com.example.realestate
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -32,12 +34,31 @@ fun MainScreen() {
 fun HouseItem(item: Int) {
     Card(
         Modifier
-            .padding(8.dp)
-            .fillMaxWidth(),
-        shape = RoundedCornerShape(10.dp),
-        elevation = 5.dp
+            .padding(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 8.dp)
+            .fillMaxWidth()
+            .height(100.dp),
+        shape = MaterialTheme.shapes.medium,
+        elevation = 2.dp,
+        backgroundColor = Color.White
     ) {
-
+        Row(modifier = Modifier.fillMaxSize()) {
+            Image(
+                modifier = Modifier
+                    .padding(14.dp)
+                    .clip(MaterialTheme.shapes.small)
+                    .align(Alignment.CenterVertically),
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "House image",
+            )
+            Spacer(modifier = Modifier
+                .width(5.dp)
+                .fillMaxHeight())
+            Column() {
+                Text(modifier = Modifier.padding(top = 14.dp), text = "$45,000", textUnit = 20.dp)
+                Text(text = "dsadasdasdasdasdasdas")
+                Text(text = "dsadasdasdasdasdasdas")
+            }
+        }
     }
 }
 
